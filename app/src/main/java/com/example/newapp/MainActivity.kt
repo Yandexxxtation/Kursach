e2a18feb07c97e149fb669d31552ec4f2d851e6d
GasilinNikita <nik.gasilin@mail.ru>
2023-03-24 02:00:17 +0300

    В аудиоплеере рандомные треки
    Осталось добавить переключение треков

--- app/src/main/java/com/example/newapp/MainActivity.kt
+++ app/src/main/java/com/example/newapp/MainActivity.kt
@@ -1,19 +1,25 @@
 package com.example.newapp
 
 import android.media.MediaPlayer
+import android.media.AsyncPlayer
 import android.os.Bundle
+import android.provider.MediaStore.Audio.Media
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
     var mPlayer: MediaPlayer? = null
-    var playButton: Button? = null
-    var pauseButton: Button? = null
-    var stopButton: Button? = null
+    var playButton: ImageButton? = null
+    var pauseButton: ImageButton? = null
+    var stopButton: ImageButton? = null
+    var RandomTrackValue = (1..2).random()
+    var stopIsActive: Boolean = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,6 +33,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun stopPlay() {
+        stopIsActive = true
         mPlayer!!.stop()
         pauseButton!!.isEnabled = false
         stopButton!!.isEnabled = false
@@ -40,6 +47,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun play(view: View?) {
+        if(stopIsActive == true) {
+            RandomTrackValue = (1..2).random()
+            when (RandomTrackValue) {
+                1 -> mPlayer = MediaPlayer.create(this, R.raw.first)
+                2 -> mPlayer = MediaPlayer.create(this, R.raw.second)
+            }
+        }
+        stopIsActive = false
         mPlayer!!.start()
         playButton!!.isVisible = false
         pauseButton!!.isVisible = true
@@ -48,6 +63,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun pause(view: View?) {
+        stopIsActive = false
         mPlayer!!.pause()
         playButton!!.isVisible = true
         pauseButton!!.isVisible = false
