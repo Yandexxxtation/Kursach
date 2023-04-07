747ce4f74d72cb2dd30d3c879366ce1ffc47e8fd
Yandexxxtation <122667473+Yandexxxtation@users.noreply.github.com>
2023-04-07 23:39:44 +0300

    commit20

---
+++ app/src/main/java/com/arhiser/todolist/screens/main/hi.java
@@ -0,0 +1,27 @@
+package com.arhiser.todolist.screens.main;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.arhiser.todolist.R;
+
+public class hi extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(hi.this, MainActivity.class);
+                hi.this.startActivity(intent);
+            }
+        });
+    }
+}
